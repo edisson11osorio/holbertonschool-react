7674e14d155933909736bd8e5d1320ac4ef776e1
edisson11osorio <osorioedisson08@gmail.com>
2022-08-22 19:31:26 -0500

    [🎉]new project react and JS [✅] adding the four project points

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+// Import jquery, (refer to Babel section for importing jQuery)
+
+import $ from "jquery";
+
+// Add 3 different <p> to the page body
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,22 @@
+// import jquery and add some instructions
+import $ from 'jquery';
+// import lodash
+import _ from 'lodash';
+
+// Here are the <p> and botton instructions to add
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button type="button">Click here to get started</button>');
+$('body').append('<p id=count></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+// bind lodash's debounce to the button
+$('button').on('click', _.debounce(updateCounter, 500));
+
+let count = 0;
+//Function that tracks the number of times the button element has been clicked
+function updateCounter() {
+	count++;
+	$('#count').text(`${count} clicks on the button`);
+}
