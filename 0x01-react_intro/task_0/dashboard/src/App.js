246e8dcf568a96e0a861d49a08ac78f89019d0a5
edisson11osorio <osorioedisson08@gmail.com>
2022-08-24 10:50:22 -0500

    [🎉]begin a new project react [✅] adding point 0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./Logo.jpg";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body>
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
+        </div>
+      </body>
+      <footer>
+        <div className="App-footer">
+          <p>Copyright 2020 - holberton School</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
